cdd3af270b6c2848299e79cfa788be1a6c61dc4b
황혁주 <nugulhie@gmail.com>
2020-11-09 16:47:06 +0900

    nove.js version html source commit
    with web masters

---
+++ app.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+const server = http.createServer(app);
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static(__dirname + "/assets"));
+app.get('/', (req, res) => {
+  res.render('intro');
+})
+app.get('/main', (req, res) => {
+  res.render('index');
+})
+app.get('/login', (req, res) => {
+    res.render('login');
+  })
+app.get('/myprofile', (req, res) => {
+    var page ="profile"; //기본 주소
+    if(req.query.page){
+        page = req.query.page
+    }
+
+    res.render('myprofile', {page: page});
+})
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
